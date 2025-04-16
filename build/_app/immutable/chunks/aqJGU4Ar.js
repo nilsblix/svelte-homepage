9863bff1a5884118be7ee18567f7d08d379dd693
nilsblix <nilsblix06@gmail.com>
2025-04-16 18:53:45 +0200

    check to see if github pages are working

--- build/_app/immutable/chunks/aqJGU4Ar.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./BVCeIcb2.js";a();
--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ export default {
             fallback: undefined,
             precompress: false,
             strict: true
-        })
+        }),
+        paths: {
+            base: process.argv.includes("dev") ? "" : "/svelte-homepage"
+        }
     }
 };
 
